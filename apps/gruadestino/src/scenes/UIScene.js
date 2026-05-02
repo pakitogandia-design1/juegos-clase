c651885a91eb5a8a4c53e64280884c68c8f3c0a8
pakitogandia-design1 <pakito.gandia@gmail.com>
2026-05-02 13:32:40 +0200

    Delete apps/gruadestino directory

--- apps/gruadestino/src/scenes/UIScene.js
+++
@@ -1,3 +0,0 @@
-// Reservada para ampliar la interfaz en futuras versiones.
-// La versión compacta integra la UI dentro de GameScene para maximizar fiabilidad en GitHub Pages.
-export default class UIScene extends Phaser.Scene { constructor(){ super('UIScene'); } }
