b69ff6c0ae6a30e9815ebdc64b5a4a5d57f6d704
pakitogandia-design1 <pakito.gandia@gmail.com>
2026-05-10 19:06:39 +0200

    Delete apps/jardicraft directory

--- apps/jardicraft/src/systems/NotificationSystem.js
+++
@@ -1,8 +0,0 @@
-export class NotificationSystem {
-  constructor(state){ this.state=state; }
-  push(title, body){
-    const n={title, body, day:this.state.day, time:Date.now()};
-    this.state.notifications.unshift(n); this.state.diary.unshift(`Dia ${this.state.day}: ${title}. ${body}`);
-    if(this.state.notifications.length>40) this.state.notifications.pop();
-  }
-}
